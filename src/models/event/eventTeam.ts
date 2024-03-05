73ba0f25bc018f8899f6df151d0c7ee719dc7174
Marcelo Benitez <btz.marclo@gmail.com>
2024-03-04 23:58:47 -0300

    Fix temporal de listado de eventos

--- src/models/event/eventTeam.ts
+++ src/models/event/eventTeam.ts
@@ -6,13 +6,13 @@ export default class MEventTeam {
     public event_id: number;
     public team_id: number;
     public active: boolean;
-    public team: MTeam;
+    public team?: MTeam;
 
     constructor(eventTeam: IEventTeam) {
         this.id = eventTeam.id;
         this.event_id = eventTeam.event_id;
         this.team_id = eventTeam.team_id;
         this.active = eventTeam.active;
-        this.team = new MTeam(eventTeam.team);
+        this.team = eventTeam.team ? new MTeam(eventTeam.team) : undefined;
     }
 }
