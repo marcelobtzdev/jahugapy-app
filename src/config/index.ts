089b5030c8161ff88c51446c09cebf3cab7ca9fe
Marcelo Benitez <btz.marclo@gmail.com>
2024-03-09 21:59:55 -0300

    Restore baseUrl config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 import { Platform } from "react-native";
 
 export default {
-    // baseUrl: Platform.OS === 'web' ? 'http://api.jahugaparaguay.com/api' : process.env.EXPO_PUBLIC_BASE_URL,
-    baseUrl: 'http://localhost/jahugapy-backend/public/api',
+    baseUrl: Platform.OS === 'web' ? 'http://api.jahugaparaguay.com/api' : process.env.EXPO_PUBLIC_BASE_URL,
 }
